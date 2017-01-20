a9957a0d946b1516c4f4a18387b11e43e3a36672
guilhermefos <guilhermefos.developer@gmail.com>
2017-01-20 11:41:26 -0200

    First Commit
    This is a project to use TDD approach with phpspec

---
+++ spec/PrimeFactorsSpec.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PrimeFactorsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('PrimeFactors');
+    }
+
+    function it_computes_the_prime_factors_of_1()
+    {
+    	$this->generate(1)->shouldReturn([]);
+    }
+
+    function it_computes_the_prime_factors_of_2()
+    {
+    	$this->generate(2)->shouldReturn([2]);
+    }
+
+    function it_computes_the_prime_factors_of_3()
+    {
+    	$this->generate(3)->shouldReturn([3]);
+    }
+
+    function it_computes_the_prime_factors_of_4()
+    {
+    	$this->generate(4)->shouldReturn([2,2]);
+    }
+
+    function it_computes_the_prime_factors_of_5()
+    {
+    	$this->generate(5)->shouldReturn([5]);
+    }
+
+    function it_computes_the_prime_factors_of_6()
+    {
+    	$this->generate(6)->shouldReturn([2,3]);
+    }
+
+    function it_computes_the_prime_factors_of_8()
+    {
+    	$this->generate(8)->shouldReturn([2,2,2]);
+    }
+
+    function it_computes_the_prime_factors_of_9()
+    {
+    	$this->generate(9)->shouldReturn([3,3]);
+    }
+
+    function it_computes_the_prime_factors_of_100()
+    {
+    	$this->generate(100)->shouldReturn([2,2,5,5]);
+    }
+}
